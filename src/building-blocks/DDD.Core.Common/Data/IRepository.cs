abd409904ecc8a6c5ebcb72cb3aee9df4bd233cc
carlos.zucheratto <carlos.zucheratto@gmail.com>
2020-09-02 21:12:07 -0300

    Implementação de interfaces de repositório

---
+++ src/building-blocks/DDD.Core.Common/Data/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using DDD.Core.Common.DomainObjects;
+
+namespace DDD.Core.Common.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+
+    }
+}
---
+++ src/building-blocks/DDD.Core.Common/Data/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace DDD.Core.Common.Data
+{
+    public interface IUnitOfWork
+    {
+        Task BeginTransaction();
+
+        Task Commit();
+
+        Task Rollback();
+    }
+}
