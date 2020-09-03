5ad5da115a62ddde775e4b27ffc95731afe89999
carlos.zucheratto <carlos.zucheratto@gmail.com>
2020-09-03 16:23:58 -0300

    Implementação da estrutura de especificação

---
+++ src/building-blocks/DDD.Core.Common/Specification/IdentitySpecification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace DDD.Core.Common.Specification
+{
+    internal sealed class IdentitySpecification<T> : Specification<T>
+    {
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            return x => true;
+        }
+    }
+}
---
+++ src/building-blocks/DDD.Core.Common/Specification/Validation/Rule.cs
@@ -0,0 +1,20 @@
+﻿namespace DDD.Core.Common.Specification.Validation
+{
+    public class Rule<T>
+    {
+        private readonly Specification<T> _specificationSpec;
+
+        public Rule(Specification<T> spec, string errorMessage)
+        {
+            _specificationSpec = spec;
+            ErrorMessage = errorMessage;
+        }
+
+        public string ErrorMessage { get; }
+
+        public bool Validate(T obj)
+        {
+            return _specificationSpec.IsSatisfiedBy(obj);
+        }
+    }
+}
