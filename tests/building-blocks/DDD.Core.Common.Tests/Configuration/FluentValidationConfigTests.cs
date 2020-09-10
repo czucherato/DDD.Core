301109d7ed6778a636906df401cce7b074a3e804
carlos.zucheratto <carlos.zucheratto@gmail.com>
2020-09-10 17:50:59 -0300

    FluentValidationConfigTests

---
+++ tests/building-blocks/DDD.Core.Common.Tests/Configuration/FluentValidationConfigTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using FluentValidation.Results;
+using DDD.Core.Common.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DDD.Core.Common.Tests.Configuration
+{
+    public class FluentValidationConfigTests
+    {
+        [Fact]
+        public void Adding_ValidationResult_To_ServiceCollection()
+        {
+            //Arrange
+            IServiceCollection services = new ServiceCollection();
+
+            //Act
+            services.AddFluentValidation();
+
+            //Assert
+            Assert.Contains(services, x => x.ServiceType == typeof(ValidationResult));
+        }
+    }
+}
