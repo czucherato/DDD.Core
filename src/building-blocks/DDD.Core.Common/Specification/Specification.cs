246a702bb307f1a81bab47448a3ee795d5ba8d0e
carlos.zucheratto <carlos.zucheratto@gmail.com>
2020-09-22 10:43:52 -0300

    Criação de SpecificationTests

--- src/building-blocks/DDD.Core.Common/Specification/Specification.cs
+++ src/building-blocks/DDD.Core.Common/Specification/Specification.cs
@@ -5,8 +5,6 @@ namespace DDD.Core.Common.Specification
 {
     public abstract class Specification<T>
     {
-        private static readonly Specification<T> All = new IdentitySpecification<T>();
-
         public bool IsSatisfiedBy(T entity)
         {
             var predicate = ToExpression().Compile();
@@ -17,19 +15,11 @@ namespace DDD.Core.Common.Specification
 
         public Specification<T> And(Specification<T> specification)
         {
-            if (this == All)
-                return specification;
-            if (specification == All)
-                return this;
-
             return new AndSpecification<T>(this, specification);
         }
 
         public Specification<T> Or(Specification<T> specification)
         {
-            if (this == All || specification == All)
-                return All;
-
             return new OrSpecification<T>(this, specification);
         }
 
--- tests/building-blocks/DDD.Core.Common.Tests/Specification/SpecificationTests.cs
+++ tests/building-blocks/DDD.Core.Common.Tests/Specification/SpecificationTests.cs
@@ -11,7 +11,7 @@ namespace DDD.Core.Common.Tests.Specification
         public void Specification_IsSatisfiedBy_Class()
         {
             //Arrange
-            var spec = new ClassBSpecification();
+            var spec = new ClassBSpecification1();
             var classB = new ClassB() { Property1 = true };
 
             //Act
@@ -20,13 +20,60 @@ namespace DDD.Core.Common.Tests.Specification
             //Assert
             Assert.True(result);
         }
+
+        [Fact]
+        public void AndSpecification_Is_Valid()
+        {
+            //Arrange
+            var classB = new ClassB() { Property1 = true, Property2 = false };
+
+            //Act
+            var result = new ClassBSpecification1().And(new ClassBSpecification2()).IsSatisfiedBy(classB);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void OrSpecification_Is_Valid()
+        {
+            //Arrange
+            var classB = new ClassB() { Property1 = true, Property2 = true };
+
+            //Act
+            var result = new ClassBSpecification1().Or(new ClassBSpecification2()).IsSatisfiedBy(classB);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void NotSpecification_Is_Valid()
+        {
+            //Arrange
+            var classB = new ClassB() { Property1 = false };
+
+            //Act
+            var result = new ClassBSpecification1().Not().IsSatisfiedBy(classB);
+
+            //Assert
+            Assert.True(result);
+        }
     }
 
-    public class ClassBSpecification : Specification<ClassB>
+    public class ClassBSpecification1 : Specification<ClassB>
     {
         public override Expression<Func<ClassB, bool>> ToExpression()
         {
             return value => value.Property1 == true;
+        }
+    }
+
+    public class ClassBSpecification2 : Specification<ClassB>
+    {
+        public override Expression<Func<ClassB, bool>> ToExpression()
+        {
+            return value => value.Property2 == false;
         }
     }
 
