8b1b15846db34a39b8b7de3a117c5f80be718751
carlos.zucheratto <carlos.zucheratto@gmail.com>
2020-09-22 17:24:56 -0300

    Criação de DomainExceptionTests

--- tests/building-blocks/DDD.Core.Common.Tests/DomainObjects/DomainExceptionTests.cs
+++ tests/building-blocks/DDD.Core.Common.Tests/DomainObjects/DomainExceptionTests.cs
@@ -1,5 +1,6 @@
 ﻿using Xunit;
 using System;
+using System.Runtime.Serialization;
 using DDD.Core.Common.DomainObjects;
 
 namespace DDD.Core.Common.Tests.DomainObjects
@@ -29,5 +30,34 @@ namespace DDD.Core.Common.Tests.DomainObjects
             //Act & Assert
             Assert.ThrowsAsync<DomainException>(() => throw new DomainException(exception.Message, exception)).GetAwaiter().GetResult();
         }
+
+        [Fact]
+        public void Calling_Protected_Constructor()
+        {
+            //Arrange
+            SerializationInfo info = new SerializationInfo(typeof(ExtendedDomainException), new FormatterConverter());
+            info.AddValue("Message", string.Empty);
+            info.AddValue("InnerException", new ArgumentException());
+            info.AddValue("HelpURL", string.Empty);
+            info.AddValue("StackTraceString", string.Empty);
+            info.AddValue("RemoteStackTraceString", string.Empty);
+            info.AddValue("RemoteStackIndex", 0);
+            info.AddValue("ExceptionMethod", string.Empty);
+            info.AddValue("HResult", 1);
+            info.AddValue("Source", string.Empty);
+            StreamingContext context = new StreamingContext();
+
+            //Act
+            var exception = new ExtendedDomainException(info, context);
+
+            //Assert
+            Assert.NotNull(exception);
+        }
+    }
+
+    public class ExtendedDomainException : DomainException
+    {
+        public ExtendedDomainException(SerializationInfo info, StreamingContext context)
+            : base (info, context) { }
     }
 }
